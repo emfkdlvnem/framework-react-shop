4d726849628b024db003e7a10c61f1e756f64c6b
YoonHyeJin <emfkdlvnem@gmail.com>
2023-05-23 05:06:51 +0900

    캐러셀 완성

--- src/views/Carousel.jsx
+++ src/views/Carousel.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faCaretLeft, faCaretRight, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import './Carousel.css';
 
 import img_shop1 from '../assets/img_shop_fashion.jpeg';
@@ -9,26 +9,28 @@ import img_shop2 from '../assets/img_shop_digital.jpeg';
 import img_shop3 from '../assets/img_shop_grocery.jpeg';
 
 const VisualCarousel = () => {
-    const slides = useMemo(() => [
-        {
-            image: img_shop1,
-            title: '물빠진 청바지',
-            description: '이제 막 도착한 패션 청바지를 구경해 보세요.',
-            link: '/fashion',
-        },
-        {
-            image: img_shop2,
-            title: '신속한 업무처리!',
-            description: '다양한 디지털 상품을 둘러보세요.',
-            link: '/digital',
-        },
-        {
-            image: img_shop3,
-            title: '신선한 식품!',
-            description: '농장 직배송으로 더욱 신선한 식료품을 만나보세요.',
-            link: '/grocery',
-        },
-    ], []);
+    const slides = useMemo(
+        () => [
+            {
+                image: img_shop1,
+                title: '물빠진 청바지',
+                description: '이제 막 도착한 패션 청바지를 구경해 보세요.',
+                link: '/fashion',
+            },
+            {
+                image: img_shop2,
+                title: '신속한 업무처리!',
+                description: '다양한 디지털 상품을 둘러보세요.',
+                link: '/digital',
+            },
+            {
+                image: img_shop3,
+                title: '신선한 식품!',
+                description: '농장 직배송으로 더욱 신선한 식료품을 만나보세요.',
+                link: '/grocery',
+            },
+        ],[]
+    );
 
     const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -42,34 +44,46 @@ const VisualCarousel = () => {
 
     useEffect(() => {
         const intervalId = setInterval(goToNextSlide, 5000);
-    
+
         return () => {
-            clearInterval(intervalId);
+        clearInterval(intervalId);
         };
     }, [goToNextSlide]);
 
     return (
         <div className="carousel">
+            <button className="carousel__button carousel__button--prev" onClick={goToPreviousSlide}>
+                <FontAwesomeIcon icon={faCaretLeft} />
+            </button>
             <div className="carousel__slide">
-                <button className="carousel__button carousel__button--prev" onClick={goToPreviousSlide}>
-                    <FontAwesomeIcon icon={faChevronLeft} />
-                </button>
                 {slides.map((slide, index) => (
-                    <div
-                        key={index}
-                        className={`carousel__image ${index === currentSlide ? 'active' : ''}`}
-                    >
-                        <img src={slide.image} alt={slide.title}/>
-                        <div className="carousel__content">
-                            <h2>{slide.title}</h2>
-                            <p>{slide.description}</p>
-                            <Link to={slide.link}>바로가기</Link>
-                        </div>
+                <div
+                    key={index}
+                    className={`carousel__image ${index === currentSlide ? 'active' : ''}`}
+                >
+                    <img src={slide.image} alt={slide.title} />
+                    <div className="carousel__content">
+                        <h2>{slide.title}</h2>
+                        <p>{slide.description}</p>
+                        <Link to={slide.link} className="link">
+                            바로가기
+                            <FontAwesomeIcon icon={faArrowRight} className="link-arrow" />
+                        </Link>
                     </div>
+                </div>
                 ))}
-                <button className="carousel__button carousel__button--next" onClick={goToNextSlide}>
-                    <FontAwesomeIcon icon={faChevronRight} />
-                </button>
+            </div>
+            <button className="carousel__button carousel__button--next" onClick={goToNextSlide}>
+                <FontAwesomeIcon icon={faCaretRight} />
+            </button>
+            <div className="carousel__indicators">
+                {slides.map((slide, index) => (
+                <div
+                    key={index}
+                    className={`carousel__indicator ${index === currentSlide ? 'active' : ''}`}
+                    onClick={() => setCurrentSlide(index)}
+                ></div>
+                ))}
             </div>
         </div>
     );
